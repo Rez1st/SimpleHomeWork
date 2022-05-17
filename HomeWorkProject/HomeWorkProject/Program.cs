502848e7065865b54c6293146573fac6922d3824
Tymur Rustamov <gtimzua@gmail.com>
2022-05-17 19:35:49 +0300

    color

--- HomeWorkProject/HomeWorkProject/Program.cs
+++ HomeWorkProject/HomeWorkProject/Program.cs
@@ -6,7 +6,21 @@ namespace HomeWorkProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(Calc.Add(10,13));
+            PrintWithColor(Calc.Add(1,2).ToString(), ConsoleColor.DarkMagenta);
+            PrintWithColor(Calc.Add(1,1321321).ToString());
+        }
+
+
+        /// <summary>
+        /// This is method to print out colored string
+        /// </summary>
+        /// <param name="str">string parameter</param>
+        /// <param name="color">color to display</param>
+        static void PrintWithColor(string str, ConsoleColor color = ConsoleColor.DarkGray)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(str);
+            Console.ResetColor();
         }
     }
 }
